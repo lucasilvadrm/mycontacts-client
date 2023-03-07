d91354706d61f9d3a2616c8c0140527f556a9af5
Lucas Silva <lucassilva.uern@gmail.com>
2023-03-07 14:40:00 -0300

    feat: criando componentes: Button, Input e Select

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
 import PageHeader from '../../components/PageHeader';
+import Button from '../../components/Button';
 
 function NewContact() {
   return (
-    <PageHeader
-      title="Novo Contato"
-    />
+    <>
+      <PageHeader
+        title="Novo Contato"
+      />
+      <Input type="text" placeholder="Nome" />
+      <Select>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+      </Select>
+      <Button type="button">Salvar alterações</Button>
+      <Button type="button" disabled>Salvar alterações</Button>
+    </>
   );
 }
 
