9258a97136335a2d5b526c0f9f47d3a4c2a5696d
Lucas Silva <lucassilva.uern@gmail.com>
2023-03-07 10:21:45 -0300

    feat: add estilo global e criação do componente header

---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Container, InputSearchContainer } from './styles';
+
+import logo from '../../assets/images/logo.svg';
+
+function Header() {
+  return (
+    <Container>
+      <img src={logo} alt="MyContacts" width={201} />
+      <InputSearchContainer>
+        <input type="text" placeholder="Pesquisar contato..." />
+      </InputSearchContainer>
+    </Container>
+  );
+}
+
+export default Header;
