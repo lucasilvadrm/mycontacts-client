322a2b4611fbd1946d37ae8478bbda5b8ed85280
Lucas Silva <lucassilva.uern@gmail.com>
2023-03-13 18:10:06 -0300

    feat: componentizando o Spinner

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,6 +1,7 @@
 import ReactDom from 'react-dom';
 import PropTypes from 'prop-types';
 import { Overlay } from './styles';
+import Spinner from '../Spinner';
 
 function Loader({ isLoading }) {
   if (!isLoading) {
@@ -9,7 +10,7 @@ function Loader({ isLoading }) {
 
   return ReactDom.createPortal(
     <Overlay>
-      <div className="loader" />
+      <Spinner size={90} />
     </Overlay>,
     document.getElementById('loader-root'),
   );
