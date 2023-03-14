0041c1ec6176b2b9cb3bd35615cd24dc41c93f73
Lucas Silva <lucassilva.uern@gmail.com>
2023-03-14 14:06:21 -0300

    feat: usando variantes no Styled Components

--- src/components/Toast/ToastMessage/styles.js
+++ src/components/Toast/ToastMessage/styles.js
@@ -1,8 +1,19 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+const containerVariants = {
+  default: css`
+    background: ${({ theme }) => theme.colors.primary.main};
+  `,
+  success: css`
+    background: ${({ theme }) => theme.colors.success};
+  `,
+  danger: css`
+    background: ${({ theme }) => theme.colors.danger.main};
+  `,
+};
 
 export const Container = styled.div`
   padding: 16px 32px;
-  background: ${({ theme }) => theme.colors.primary.main};
   color: #fff;
   border-radius: 4px;
   box-shadow: 0px 20px 20px -16px rgba(0, 0, 0, 0.25);
@@ -10,6 +21,8 @@ export const Container = styled.div`
   align-items: center;
   justify-content: center;
   gap: 8px;
+
+  ${({ type }) => containerVariants[type] || containerVariants.default}
 
   & + & {
     margin-top: 12px;
