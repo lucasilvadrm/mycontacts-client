eaa0f60e7349c05e73fadcd61e46fc02ea3e35c3
Lucas Silva <lucassilva.uern@gmail.com>
2024-01-27 11:41:32 -0300

    feat: implementendo container/presentation pattern

---
+++ src/pages/EditContact/Container.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
+import ContactsService from '../../services/ContactsService';
+import toast from '../../utils/toast';
+import useSafeAsyncAction from '../../hooks/useSafeAsyncAction';
+import Presentation from './Presentation';
+
+function Container() {
+  const { id } = useParams();
+  const history = useHistory();
+  const safeAsyncAction = useSafeAsyncAction();
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [contactName, setContactName] = useState('');
+  const contactFormRef = useRef(null);
+
+  useEffect(() => {
+    const loadContact = async () => {
+      try {
+        const contact = await ContactsService.getContactById(id);
+
+        // executa código se o componente estiver montado
+        safeAsyncAction(() => {
+          contactFormRef.current.setFieldsValues(contact);
+
+          setIsLoading(false);
+          setContactName(contact.name);
+        });
+      } catch {
+        safeAsyncAction(() => {
+          history.push('/');
+          toast({
+            type: 'danger',
+            text: 'Contato não encontrado!',
+          });
+        });
+      }
+    };
+
+    loadContact();
+  }, [history, id, safeAsyncAction]);
+
+  const handleSubmit = async (contact) => {
+    try {
+      const updatedContact = await ContactsService.updateContact(id, contact);
+
+      setContactName(updatedContact.name);
+      toast({
+        type: 'success',
+        text: 'Editado com sucesso!',
+        duration: 3000,
+      });
+    } catch {
+      toast({
+        type: 'danger',
+        text: 'Erro ao editar o contato!',
+      });
+    }
+  };
+
+  return (
+    <Presentation
+      contactFormRef={contactFormRef}
+      contactName={contactName}
+      isLoading={isLoading}
+      onSubmit={handleSubmit}
+    />
+  );
+}
+
+export default Container;
