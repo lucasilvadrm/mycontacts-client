22596b6012009f34e0ed57fba193bd06d7511665
Lucas Silva <lucassilva.uern@gmail.com>
2024-01-29 14:27:33 -0300

    style: criando fadeIn e scaleIn para modal

--- src/components/Modal/styles.js
+++ src/components/Modal/styles.js
@@ -1,4 +1,27 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const fadeIn = keyframes`
+  from {
+    /* estilos iniciais */
+    opacity: 0;
+  }
+
+  to {
+    opacity: 1;
+    /* estilos finais (como o elemento vai permanecer em tela) */
+  }
+`;
+
+const scaleIn = keyframes`
+/* o 0 ou 1 do scale diz respeito a porcentage do tamanho do elemento */
+  from {
+    transform: scale(0);
+  }
+
+  to {
+    transform: scale(1);
+  }
+`;
 
 export const Overlay = styled.div`
   background: rgba(0, 0, 0, 0.6);
@@ -11,6 +34,9 @@ export const Overlay = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  /* forwards: mantém os estilos do to (fadeIn) */
+  /* animation: ${fadeIn} 0.3s forwards; */
+  animation: ${fadeIn} 0.3s;
 `;
 
 export const Container = styled.div`
@@ -20,6 +46,7 @@ export const Container = styled.div`
   border-radius: 4px;
   padding: 24px;
   box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+  animation: ${scaleIn} 0.3s;
 
   & > h1 {
     font-size: 22px;
