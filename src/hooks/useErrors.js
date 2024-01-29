4def1cdd0c6a5e879c7e1a6cf117853d25c9559d
Lucas Silva <lucassilva.uern@gmail.com>
2024-01-29 10:10:50 -0300

    refactor: adicionando useCallback nas funções do hook

--- src/hooks/useErrors.js
+++ src/hooks/useErrors.js
@@ -1,27 +1,36 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 export const useErrors = () => {
   const [errors, setErrors] = useState([]);
 
-  const errorAlreadyExists = ({ field }) => errors.find((error) => error.field === field);
+  const errorAlreadyExists = useCallback(
+    ({ field }) => errors.find((error) => error.field === field),
+    [errors],
+  );
 
-  const getErrorMessageByFieldName = (fieldName) => {
+  const getErrorMessageByFieldName = useCallback((fieldName) => {
     const foundError = errorAlreadyExists({ field: fieldName });
     return foundError?.message;
-  };
+  }, [errorAlreadyExists]);
 
-  const setError = ({ field, message }) => {
-    if (errorAlreadyExists({ field })) return;
-    setErrors((prev) => [...prev, { field, message }]);
-  };
+  const setError = useCallback(
+    ({ field, message }) => {
+      if (errorAlreadyExists({ field })) return;
+      setErrors((prev) => [...prev, { field, message }]);
+    },
+    [errorAlreadyExists],
+  );
 
-  const removeError = ({ field }) => {
+  const removeError = useCallback(({ field }) => {
     setErrors((prev) => prev.filter((error) => error.field !== field));
-  };
+  }, []);
 
   const hasErrors = errors.length > 0;
 
   return {
-    setError, removeError, hasErrors, getErrorMessageByFieldName,
+    setError,
+    removeError,
+    hasErrors,
+    getErrorMessageByFieldName,
   };
 };
